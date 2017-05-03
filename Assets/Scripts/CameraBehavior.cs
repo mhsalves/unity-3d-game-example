0351d79c1425f70e9fd6546c2b54f44c22951a9e
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-05-03 17:32:02 -0400

    + Standard Assets + Assets de Plataformas + Objetos + Scripts de Camera + Prefabs de Plataformas

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+	public Transform player; //Informa que pro script funcionar ele precisa de um alvo, no caso o personagem.
+
+	// Update is called once per frame
+	void Update () {
+
+		var posicaoNovaParaACamera = this.player.position; //Carrego a posicao do player para uma variavel
+		posicaoNovaParaACamera.z = this.transform.position.z; //Mudo a posicao "z" da variavel pra a da Camera, mantendo a posicao "z" da Camera.
+
+		this.transform.position = posicaoNovaParaACamera; //Mudo a posicao atual da Camera para a posicao nova.
+	}
+}
---
+++ Assets/Scripts/CameraComLimiteBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraComLimiteBehavior : MonoBehaviour {
+
+	public Transform player; //Informa que pro script funcionar ele precisa de um alvo, no caso o personagem.
+
+	public float alturaMinima = 1.56f;
+	public float alturaMaxima = 4.9f;
+
+	// Update is called once per frame
+	void Update () {
+
+		var posicaoNovaParaACamera = this.player.position; //Carrego a posicao do player para uma variavel
+		posicaoNovaParaACamera.z = this.transform.position.z; //Mudo a posicao "z" da variavel pra a da Camera, mantendo a posicao "z" da Camera.
+
+		//Validar Alturas --------
+		if (posicaoNovaParaACamera.y <= alturaMinima) {
+			posicaoNovaParaACamera.y = alturaMinima;
+		}
+
+		if (posicaoNovaParaACamera.y >= alturaMaxima) {
+			posicaoNovaParaACamera.y = alturaMaxima;
+		}
+		//------------------------
+
+		this.transform.position = posicaoNovaParaACamera; //Mudo a posicao atual da Camera para a posicao nova.
+	}
+}
